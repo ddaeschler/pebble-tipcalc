04b547a81ed75c1ec2fefc07cb3bf88fef6b3c87
David Daeschler <david.daeschler@gmail.com>
2014-03-09 18:30:22 -0400

    Remove all floating point ops and replace with fixed point. Allow editing of cents portion of total

--- src/tip_calc.c
+++ src/tip_calc.c
@@ -25,6 +25,7 @@
 static const int DISP_BUFFER_LEN = 32;
 
 static Window *window;
+static AppTimer* timer = 0;
 
 static TextLayer* bill_amount_layer;
 static TextLayer* bill_amount_label;
@@ -38,10 +39,15 @@ static TextLayer* tip_amount_label;
 static TextLayer* total_layer;
 static TextLayer* total_label;
 
+//amounts are stored as an integer representing tenths of a cent
+static const unsigned int TO_DOLLARS = 1000;
+static const unsigned int TO_CENTS = 10;
+
+
 static unsigned int bill_amount;
 static unsigned int tip_percentage;
-static float total_amt;
-static float tip_amt;
+static unsigned int total_amount;
+static unsigned int tip_amount;
 
 static char* bill_amt_buffer;
 static char* tip_percent_buffer;
@@ -49,16 +55,28 @@ static char* tip_amt_buffer;
 static char* total_amt_buffer;
 
 typedef void (*StateEntry)();
+typedef void (*HandleTimer)();
 typedef struct state
 {
 	ClickHandler up_single_click_handler;
 	ClickHandler down_single_click_handler;
 	ClickHandler select_click_handler;
+	HandleTimer timer_handler;
 	StateEntry state_entry;
 	
 } state;
+
+typedef struct dollars_cents
+{
+	unsigned int dollars;
+	unsigned int cents;
+	
+} dollars_cents;
 	
 static state* current_state;
+
+typedef enum {NONE, DOLLARS, CENTS} blinktype;
+blinktype blink = NONE;
 
 /**
  * State prototypes and defs
@@ -66,53 +84,112 @@ static state* current_state;
 static void tipamt_select_single_click_handler(ClickRecognizerRef recognizer, void *context);
 static void tipamt_up_single_click_handler(ClickRecognizerRef recognizer, void *context);
 static void tipamt_down_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void tipamt_timer_handler(void* data);
 static void tipamt_state_entry();
 
 static state edit_tip_amount_state = 
 	{ 	.up_single_click_handler = tipamt_up_single_click_handler, 
 		.down_single_click_handler = tipamt_down_single_click_handler,
 		.select_click_handler = tipamt_select_single_click_handler,		
-		.state_entry = tipamt_state_entry 								};
+		.state_entry = tipamt_state_entry, 								
+		.timer_handler = tipamt_timer_handler									};
 
-static void billamt_select_single_click_handler(ClickRecognizerRef recognizer, void *context);
-static void billamt_up_single_click_handler(ClickRecognizerRef recognizer, void *context);
-static void billamt_down_single_click_handler(ClickRecognizerRef recognizer, void *context);
-static void billamt_state_entry();
+static void billamt_dollars_select_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void billamt_dollars_up_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void billamt_dollars_down_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void billamt_dollars_timer_handler(void* data);
+static void billamt_dollars_state_entry();
 
-static state edit_bill_amount_state = 
-	{ 	.up_single_click_handler = billamt_up_single_click_handler, 
-		.down_single_click_handler = billamt_down_single_click_handler,
-		.select_click_handler = billamt_select_single_click_handler,
-		.state_entry = billamt_state_entry								};
-		
+static state edit_bill_amount_dollars_state = 
+	{ 	.up_single_click_handler = billamt_dollars_up_single_click_handler, 
+		.down_single_click_handler = billamt_dollars_down_single_click_handler,
+		.select_click_handler = billamt_dollars_select_single_click_handler,
+		.state_entry = billamt_dollars_state_entry,								
+		.timer_handler = billamt_dollars_timer_handler							};
+
+static void billamt_cents_select_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void billamt_cents_up_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void billamt_cents_down_single_click_handler(ClickRecognizerRef recognizer, void *context);
+static void billamt_cents_timer_handler(void* data);
+static void billamt_cents_state_entry();
+
+static state edit_bill_amount_cents_state = 
+	{ 	.up_single_click_handler = billamt_cents_up_single_click_handler, 
+		.down_single_click_handler = billamt_cents_down_single_click_handler,
+		.select_click_handler = billamt_cents_select_single_click_handler,
+		.state_entry = billamt_cents_state_entry,							
+		.timer_handler = billamt_cents_timer_handler							};
+
 
 /**
  * Core functions
  */
-void float_to_string(char* buffer, int bufferSize, double number)
-{
-	char decimalBuffer[5];
+static void format_dollars_cents_blink(dollars_cents ds, char* out, blinktype mblink) {
+	if (ds.cents >= 10) {
+		if (mblink == NONE) {
+			snprintf(out, DISP_BUFFER_LEN, "$%i.%i", ds.dollars, ds.cents);
 
-	snprintf(buffer, bufferSize, "%d", (int)number);
-	strcat(buffer, ".");
+		} else if (mblink == DOLLARS) {
+			snprintf(out, DISP_BUFFER_LEN, ".%i", ds.cents);
 
-	snprintf(decimalBuffer, 5, "%02d", (int)((double)(number - (int)number) * (double)100));
-	strcat(buffer, decimalBuffer);
+		} else if (mblink == CENTS) {
+			snprintf(out, DISP_BUFFER_LEN, "$%i.__", ds.dollars);
+		}
+
+	} else {
+		if (mblink == NONE) {
+			snprintf(out, DISP_BUFFER_LEN, "$%i.0%i", ds.dollars, ds.cents);
+
+		} else if (mblink == DOLLARS) {
+			snprintf(out, DISP_BUFFER_LEN, ".0%i", ds.cents);
+
+		} else if (mblink == CENTS) {
+			snprintf(out, DISP_BUFFER_LEN, "$%i.__", ds.dollars);
+		}
+	}
+}
+
+static void format_dollars_cents(dollars_cents ds, char* out) {
+	format_dollars_cents_blink(ds, out, NONE); 
+}
+
+static dollars_cents to_dollars_cents(unsigned int num) {
+	unsigned int dollars = num / TO_DOLLARS;
+	unsigned int cents = (num - (dollars * TO_DOLLARS)) / TO_CENTS;
+	
+	return (dollars_cents) { .dollars = dollars, .cents = cents };
+}
+
+static int round_up(int numToRound, int multiple) { 
+	if (multiple == 0) 
+	{ 
+		return numToRound; 
+	} 
+
+	int remainder = numToRound % multiple;
+	if (remainder == 0) 
+	{
+		return numToRound;
+	}
+	
+	return numToRound + multiple - remainder;
 }
 
 static void update_total_display() {
-	float_to_string(total_amt_buffer + 1, DISP_BUFFER_LEN, total_amt);
-	total_amt_buffer[0] = '$';
+	dollars_cents ds = to_dollars_cents(total_amount);
+	format_dollars_cents(ds, total_amt_buffer);
 	
 	text_layer_set_text(total_layer, total_amt_buffer);
 }
 
 static void recalc_total() {
-	total_amt = bill_amount * (1 + (tip_percentage / 100.0f));
+	total_amount = bill_amount + tip_amount;
 }
 
 static void update_bill_amount_display() {
-	snprintf(bill_amt_buffer, DISP_BUFFER_LEN, "$%i.00", bill_amount);
+	dollars_cents ds = to_dollars_cents(bill_amount);
+	format_dollars_cents_blink(ds, bill_amt_buffer, blink);
+	
 	text_layer_set_text(bill_amount_layer, bill_amt_buffer);
 }
 
@@ -122,12 +199,14 @@ static void update_tip_percentage_display() {
 }
 
 static void recalc_tip_amount() {
-	tip_amt = bill_amount * (tip_percentage / 100.0f);
+	//1000 = 1, 100 = .1, 10 = 0.01
+	//multiply the tip amount * bill_amount then scale (divide) by 100 (percent)
+	tip_amount = round_up((bill_amount * tip_percentage) / (TO_DOLLARS / 10), TO_CENTS);
 }
 
 static void update_tip_amount_display() {
-	float_to_string(tip_amt_buffer + 1, DISP_BUFFER_LEN, tip_amt);
-	tip_amt_buffer[0] = '$';
+	dollars_cents ds = to_dollars_cents(tip_amount);
+	format_dollars_cents(ds, tip_amt_buffer);
 	
 	text_layer_set_text(tip_amount_layer, tip_amt_buffer);
 }
@@ -138,6 +217,58 @@ static void calculate_and_display_new_amounts() {
 	
 	recalc_total();
 	update_total_display();
+}
+
+
+/**
+ * Timer functions
+ */
+
+const int TIMER_PERIOD = 500;
+const int MAX_NUM_BLINKS = 10;
+int num_blinks = 0;
+
+static void handle_timer(void* data);
+
+static void reschedule_timer(bool resetBlinkCounter) {
+	if (timer != 0) 
+	{
+		app_timer_cancel(timer);
+		timer = 0;
+	}
+	
+	bool reschedule = false;
+	if (resetBlinkCounter) 
+	{
+		num_blinks = 0;
+		reschedule = true;
+	}
+	else
+	{
+		if (num_blinks < MAX_NUM_BLINKS)
+		{
+			num_blinks++;
+			reschedule = true;
+		}
+	}
+	
+	if (reschedule) 
+	{
+		timer = app_timer_register(TIMER_PERIOD, &handle_timer, NULL);
+	}
+	else
+	{
+		blink = NONE;
+		update_bill_amount_display();
+	}
+}
+
+static void handle_timer(void* data) {
+	timer = 0;
+	current_state->timer_handler(data);
+
+	//always reschedule the timer
+	reschedule_timer(false);
 }
 
 
@@ -152,11 +283,15 @@ static void tipamt_state_entry() {
 	text_layer_set_text_color(tip_percentage_label, GColorWhite);
 	text_layer_set_background_color(bill_amount_label, GColorWhite);
 	text_layer_set_text_color(bill_amount_label, GColorBlack);
+	
+	//no blink here
+	blink = NONE;
+	update_bill_amount_display();
 }
 
 static void tipamt_select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
 	//switch to the bill amount
-	current_state = &edit_bill_amount_state;
+	current_state = &edit_bill_amount_dollars_state;
 	current_state->state_entry();
 }
 
@@ -176,43 +311,122 @@ static void tipamt_down_single_click_handler(ClickRecognizerRef recognizer, void
 	calculate_and_display_new_amounts();
 }
 
+static void tipamt_timer_handler(void* data) {
+	if (blink != NONE) {
+		blink = NONE;
+		update_bill_amount_display();
+	}
+}
 		
 
 /**
- * State handlers for the billing amount adjustment state
+ * State handlers for the billing amount dollars adjustment state
  */
-static void billamt_state_entry() {
+static void billamt_dollars_state_entry() {
 	//when we enter the billamt state, we need to update the colors to
 	//highlight the billamt label, and de-highlight the tipamt label
 	text_layer_set_background_color(bill_amount_label, GColorBlack);
 	text_layer_set_text_color(bill_amount_label, GColorWhite);
 	text_layer_set_background_color(tip_percentage_label, GColorWhite);
 	text_layer_set_text_color(tip_percentage_label, GColorBlack);
+	
+	//initial blink type is none
+	blink = NONE;
+	update_bill_amount_display();
+	
+	reschedule_timer(true);
 }
 
-static void billamt_select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void billamt_dollars_select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	//switch to cents
+	current_state = &edit_bill_amount_cents_state;
+	current_state->state_entry();
+}
+
+static void billamt_dollars_up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	blink = NONE;
+	bill_amount += TO_DOLLARS;
+	
+	update_bill_amount_display();
+	calculate_and_display_new_amounts();
+	
+	reschedule_timer(true);
+}
+
+static void billamt_dollars_down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	blink = NONE;
+	if (bill_amount > TO_DOLLARS) bill_amount -= TO_DOLLARS;
+	else bill_amount = 0;
+	
+	update_bill_amount_display();
+	calculate_and_display_new_amounts();
+	
+	reschedule_timer(true);
+}
+
+static void billamt_dollars_timer_handler(void* data) {
+	if (blink == NONE) blink = DOLLARS;
+	else blink = NONE;
+	
+	update_bill_amount_display();
+}
+
+
+/**
+ * State handlers for the billing amount cents adjustment state
+ */
+static void billamt_cents_state_entry() {
+	//when we enter the billamt state, we need to update the colors to
+	//highlight the billamt label, and de-highlight the tipamt label
+	text_layer_set_background_color(bill_amount_label, GColorBlack);
+	text_layer_set_text_color(bill_amount_label, GColorWhite);
+	text_layer_set_background_color(tip_percentage_label, GColorWhite);
+	text_layer_set_text_color(tip_percentage_label, GColorBlack);
+	
+	//initial blink type is NONE
+	blink = NONE;
+	update_bill_amount_display();
+	
+	reschedule_timer(true);
+}
+
+static void billamt_cents_select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
 	//switch to the tip percentage
 	current_state = &edit_tip_amount_state;
 	current_state->state_entry();
 }
 
-static void billamt_up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
-	++bill_amount;
+static void billamt_cents_up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	blink = NONE;
+	bill_amount += TO_CENTS;
 	
 	update_bill_amount_display();
 	calculate_and_display_new_amounts();
+	
+	reschedule_timer(true);
 }
 
-static void billamt_down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
-	if (bill_amount > 0) --bill_amount;
+static void billamt_cents_down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	blink = NONE;
+	if (bill_amount > TO_CENTS) bill_amount -= TO_CENTS;
+	else bill_amount = 0;
 	
 	update_bill_amount_display();
 	calculate_and_display_new_amounts();
+	
+	reschedule_timer(true);
+}
+
+static void billamt_cents_timer_handler(void* data) {
+	if (blink == NONE) blink = CENTS;
+	else blink = NONE;
+	
+	update_bill_amount_display();
 }
 
 		
 /**
- * Base click handling 
+ * Base click and timer handling 
  */
 static void do_select_single_click(ClickRecognizerRef recognizer, void *context) {
 	current_state->select_click_handler(recognizer, context);
@@ -311,7 +525,7 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
-  	bill_amount = 20;
+  	bill_amount = 20 * TO_DOLLARS;
 	tip_percentage = 15;
 	
 	bill_amt_buffer = (char*)malloc(DISP_BUFFER_LEN);
@@ -319,7 +533,7 @@ static void init(void) {
 	tip_amt_buffer = (char*)malloc(DISP_BUFFER_LEN);
 	total_amt_buffer = (char*)malloc(DISP_BUFFER_LEN);
 	
-	current_state = &edit_bill_amount_state;
+	current_state = &edit_bill_amount_dollars_state;
 	
 	window = window_create();
 	window_set_click_config_provider(window, click_config_provider);
@@ -342,9 +556,9 @@ static void deinit(void) {
 
 int main(void) {
 	init();
-
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
+	
+	reschedule_timer(true);
 	app_event_loop();
+	
 	deinit();
 }
